167a4bdc472210df3c24748c04c44224d5fbf5da
Pedro Lucas <pedro.lucas.fl@hotmail.com>
2022-07-06 20:29:08 -0300

    Ainda mais aulas de DAO orientado a objeto

--- index.php
+++ index.php
@@ -6,10 +6,22 @@ require_once("config.php");
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+// Carrega um usuario
+//$root = new Usuario();
+//$root->loadById(4);
+//echo $root;
 
-$root->loadById(4);
+//Carrega uma lista de usuario
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuario procurando com login
+//echo json_encode($search);
+
+//Carrega usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user","!@#$");
+
+echo $usuario;
 
  ?>
